5033ddde6e02d4db97b7124ca0935a8117b08a67
evilchili <evilchili@gmail.com>
2024-03-01 16:02:30 -0800

    fixing path bug, supporting python 3.8 for old server

--- croaker/pidfile.py
+++ croaker/pidfile.py
@@ -7,7 +7,7 @@ from daemon import pidfile as _pidfile
 
 
 def pidfile(pidfile_path: Path, terminate_if_running: bool = True):
-    pf = _pidfile.TimeoutPIDLockFile(pidfile_path, 30)
+    pf = _pidfile.TimeoutPIDLockFile(str(pidfile_path.expanduser()), 30)
     pid = pf.read_pid()
     if pid and terminate_if_running:
         try:
